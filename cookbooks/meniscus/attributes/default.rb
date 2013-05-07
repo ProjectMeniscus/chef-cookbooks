85dce09c6dd724302058112ae93570e5c6ea4b57
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-07 15:57:54 -0500

    completed mwniscus worker recipe

--- cookbooks/meniscus/attributes/default.rb
+++ cookbooks/meniscus/attributes/default.rb
@@ -1,4 +1,3 @@
 default[:meniscus][:personality] = "cbootstrap"
-default[:meniscus][:coordinator_ip] = "127.0.0.1"
 default[:meniscus][:cluster_name] = "meniscus"
 default[:meniscus][:port] = "8080"
--- cookbooks/meniscus/recipes/default.rb
+++ cookbooks/meniscus/recipes/default.rb
@@ -21,6 +21,8 @@ include_recipe 'apt'
 include_recipe "python::#{node['python']['install_method']}"
 include_recipe "python::pip"
 
+
+#pip install all of the dependencies for meniscus
 %w(falcon wsgiref pymongo requests 
   iso8601 eventlet oslo.config uWSGI
   https://github.com/ProjectMeniscus/portal/blob/release/Meniscus%20Portal-0.1.tar.gz?raw=true).each do |pkg|
@@ -29,36 +31,31 @@ include_recipe "python::pip"
   end
 end
 
-#remote_file "/tmp/meniscus_0.1.302-1_all.deb" do
-#  source "http://50.56.176.80:8000/meniscus_0.1.302-1_all.deb"
-#  notifies :run, "bash[install_program]", :immediately
-#end
-
-#bash "install_program" do
-#  user "root"
-#  cwd "/tmp"
-#  code <<-EOH
-#    dpkg -i /tmp/meniscus_0.1.302-1_all.deb
-#  EOH
-#  action :nothing
-#end
-
+#Add Meniscus repository
 apt_repository "ProjectMeniscus" do
   uri "http://198.61.162.39"
   distribution "squeeze"
   components ["main"]
 end
 
+#update app definitions
 execute "apt-get update" do
   command "apt-get update"
   action :run
 end
 
+#install meniscus from repo
 package "meniscus" do
   action :install
   options "--force-yes"
 end
 
+service "meniscus" do
+  supports :restart => true
+  action :enable
+end
+
+#configure firewall rules to open a port for meniscus
 ruby_block "edit iptables.rules" do
   block do
     meniscus_port_rule = "-A TCP -p tcp -m tcp --dport #{node[:meniscus][:port]} -j ACCEPT"
@@ -69,6 +66,7 @@ ruby_block "edit iptables.rules" do
   end
 end
 
+#apply new firewall rules immediately
 bash "iptables-restore" do
   user "root"
   code <<-EOH
@@ -77,59 +75,110 @@ bash "iptables-restore" do
   action :nothing
 end
 
+#if this is the first worker for the meniscus cluster
+#set the default python app in an environmental variable 
+#and make the worker a coordinator
 if node[:meniscus][:personality] == "cbootstrap"
   ENV["WORKER_PERSONA"] ="meniscus.personas.cbootstrap.app"
-  puts "SETTING ENVIRONMENT VARIABLE"
+  node.set[:meniscus][:personality] = "coordinator"
 end
 
-
-if ["cbootstrap", "coordinator"].include? node[:meniscus][:personality]
-	db_nodes = search(:node, "mmongo_replset_name:rs0")
-	
-elsif ["storage"].include? node[:meniscus][:perosnality]
+#if the worker is a coordinator, search chef server for the 
+#mongo database nodes that are members of the configuration replicaset
+if ["coordinator"].include? node[:meniscus][:personality]
 	db_nodes = search(:node, "mmongo_replset_name:rs0")
 
+#if the worker is a storage node, then search chef server for the
+#mongo database nodes that are members of the log storage replicaset
+elsif ["storage"].include? node[:meniscus][:personality]
+	db_nodes = search(:node, "mmongo_replset_name:rs0")
+
+#else, this worker will not need to talk to a database
 else 
 	db_nodes = []
 end
 
+#create a new array containg only the ip_address:mongo_port_no for each of 
+#the selected databse nodes
 db_ip = []
 db_nodes.each do |db_node|
 	db_ip.push([db_node[:ipaddress], db_node[:mmongo][:port]].join(':'))
 end
 
+#create the meniscus configuration file
+#use the db_ip array to create the list of mongo servers needed in the conf file
 template "/etc/meniscus/meniscus.conf" do
     source "meniscus.conf.erb"
     variables(
       :mongo_servers => db_ip.join(',')
     )
+    notifies :restart, "service[meniscus]", :immediately
 end
 
-service "meniscus" do
-	action :restart
+#restart meniscus to load settings from the new conf file
+#service "meniscus" do
+#	action :restart
+#end
+
+if not node[:meniscus][:coordinator_ip] or node[:meniscus][:coordinator_ip].empty?
+  ruby_block "assign coordinator" do
+    block do
+      #search chef-server for a list of all coordinators
+      coordinator_nodes = search(:node, "meniscus_personality:coordinator")
+      if coordinator_nodes.count == 0
+        node.set[:meniscus][:coordinator_ip] = node[:ipaddress]
+
+      else
+        coordinator_info = Array.new
+
+        coordinator_nodes.each do |coordinator|
+
+          #get the coordinator's ip address
+          coordinator_ip = coordinator[:ipaddress]
+          #search for a list of nodes that are assigned to this coordinator
+          member_nodes = search(:node, "meniscus_coordinator_ip:#{coordinator_ip}")
+          #create a stats item that list the coordinator's ip address and the number of member nodes
+          coordinator_info.push({:coordinator_ip => coordinator_ip, :node_count => member_nodes.count})
+        end
+
+        #sort the coordinator array by the number of nodes assigned, 
+        #and then select the first coordinator in the array
+        coordinator_info.sort_by { |hsh| hsh[:node_count] }
+        selected_coordinator = coordinator_info[0]
+        node.set[:meniscus][:coordinator_ip] = selected_coordinator[:coordinator_ip]
+
+      end
+
+    end
+  end
 end
 
-
+#install ruby json support 
 chef_gem "json" do
   action :install
 end
 
+#give the new worker its configuration by making an http POST
 ruby_block "post configuration" do
   block do
     require 'rubygems'
     require 'net/http'
     require 'json'
     
+    #build the bosy of the post as a json string
     body = {
       "api_secret" => "87188ab51cdhg6efeeee",
-      "coordinator_uri"  =>  "http://#{node[:ipaddress]}:#{node[:meniscus][:port]}/v1",
-      "personality"  =>  "coordinator"
+      "coordinator_uri"  =>  "http://#{node[:meniscus][:coordinator_ip]}:#{node[:meniscus][:port]}/v1",
+      "personality"  =>  node[:meniscus][:personality]
     }.to_json
 
     print body
 
+    #build the POST request objet
     req = Net::HTTP::Post.new("/v1/pairing/configure", initheader = {'Content-Type' =>'application/json'})
     req.body = body
+
+    #make the POST request
     response = Net::HTTP.new(node[:ipaddress], node[:meniscus][:port]).start {|http| http.request(req) }
     puts "Response #{response.code} #{response.message}:
           #{response.body}"
