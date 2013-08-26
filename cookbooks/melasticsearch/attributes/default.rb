faacab8fda14f7870a4ddffb0f6a51b33edab07b
Steven Gonzales <stevendgonzales@gmail.com>
2013-08-25 22:16:44 -0500

    update elasticsearch recipe to use newrelic plugin

--- cookbooks/melasticsearch/attributes/default.rb
+++ cookbooks/melasticsearch/attributes/default.rb
@@ -1,7 +1,7 @@
 
 default[:elasticsearch][:port] = "9200"
 default[:elasticsearch][:cluster_port] = "9300"
-default[:elasticsearch][:es_heap_size] = "1g"
+default[:elasticsearch][:es_heap_size] = "256m"
 default[:elasticsearch][:plugin_head] = false
 
 default[:elasticsearch][:discovery_zen_ping_unicast_hosts] = nil
@@ -13,6 +13,9 @@ default[:elasticsearch][:gateway_expected_nodes] = 24
 default[:elasticsearch][:discovery_zen_minimum_master_nodes] = 3
 default[:elasticsearch][:discovery_zen_ping_multicast_enabled] = false
 default[:elasticsearch][:unicast_host_for_every_n_nodes] = 6
+default[:elasticsearch][:use_newrelic] = false
+default[:elasticsearch][:newrelic_plugin_url] = ""
+default[:elasticsearch][:newrelic_plugin_jar] = "elasticsearch-newrelic-master-0.0.1.jar"
 
 
 
