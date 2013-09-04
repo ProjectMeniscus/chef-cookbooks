6d00ac5a41b60ae99b3964f11530bb93297b05c5
Taz Elkikhia <aelkikhia@gmail.com>
2013-09-04 13:30:54 -0500

    Update metadata.rb

--- cookbooks/ntp/metadata.rb
+++ cookbooks/ntp/metadata.rb
@@ -1,9 +1,7 @@
 name             'ntp'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Rackspace'
+maintainer_email 'logging@lists.rackspace.com'
 license          'All rights reserved'
 description      'Installs/Configures ntp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
-depends "apt"
