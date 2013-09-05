2bb1162b618f431bae643ba19ef3d8fa97737b1b
Steven Gonzales <stevendgonzales@gmail.com>
2013-09-05 08:07:51 -0500

    remove dev-template cookbook

--- cookbooks/dev-template/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'dev-template'
-maintainer       'Rackspace'
-maintainer_email 'steven.gonzales@rackspace.com'
-license          'APLv2'
-description      'Installs/Configures dev-template'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
