bb5d2535fa8d6f72d27718c433ff2a4de6376538
Taz <aelkikhia@gmail.com>
2013-09-30 17:20:58 -0500

    remove old kibana cookbooks

--- cookbooks/chef-kibana/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# CHANGELOG for kibana
-
-This file is used to list changes made in each version of kibana.
-
-## 1.0.0:
-
-* Initial release of kibana
-
-
--- cookbooks/mkibana/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'mkibana'
-maintainer       'Rackspace'
-maintainer_email 'almutaz.el-kikhia@rackspace.com'
-license          'All rights reserved'
-description      'Installs/Configures mkibana_test'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends "git"
