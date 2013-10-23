0771acfd60efd7bb216b5862a31635d2fd56af31
Steven Gonzales <stevendgonzales@gmail.com>
2013-10-23 15:01:28 -0500

    remove old es cookbook

--- cookbooks/melasticsearch/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'melasticsearch'
-maintainer       'Steven Gonzales'
-maintainer_email 'steven.gonzales@rackspace.com'
-license          'Apache 2.0'
-description      'Installs/Configures elasticsearch for the Meniscus application'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
