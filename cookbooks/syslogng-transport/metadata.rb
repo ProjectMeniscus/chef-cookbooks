6c439d5ac2359381a6b64173faf3e095fbc61814
Steven Gonzales <stevendgonzales@gmail.com>
2013-11-25 19:41:02 -0600

    adding syslogng-transport cookbook

---
+++ cookbooks/syslogng-transport/metadata.rb
@@ -0,0 +1,9 @@
+name             'syslogng-transport'
+maintainer       'Steven Gonzales'
+maintainer_email 'steven.gonzales@rackspace.com'
+license          'Apache 2.0'
+description      'Installs/Configures syslogng-transport'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'libzmq'
---
+++ cookbooks/syslogng-transport/recipes/default.rb
@@ -0,0 +1,56 @@
+#
+# Cookbook Name:: syslogng-transport
+# Recipe:: default
+#
+# Copyright (C) 2013 Steven Gonzales
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'libzmq'
+
+package "python-zmq" do
+  action :install
+end
+
+service "rsyslog" do
+  action [ :disable, :stop ]
+end
+
+package "syslog-ng" do
+  action :install
+end
+
+package "syslog-ng-mod-json" do
+  action :install
+end
+
+remote_file "/etc/syslog-ng/transport_zmq.py" do
+  source "https://raw.github.com/ProjectMeniscus/transport/master/transport/transport_zmq.py"
+end
+
+template "/etc/syslog-ng/syslog-ng.conf" do
+  source "syslog-ng.conf.erb"
+  notifies :restart, "service[syslog-ng]"
+end
+
+template "/etc/syslog-ng/modules.conf" do
+  source "modules.conf.erb"
+  notifies :restart, "service[syslog-ng]"
+end
+
+service "syslog-ng" do
+  supports :restart => true, :status => true
+  action :enable
+end
+
