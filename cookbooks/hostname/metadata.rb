48aa0a52732b5a8379f8a8e9063de3543aba9a3a
Steven Gonzales <stevendgonzales@gmail.com>
2013-12-02 17:20:04 -0600

    remove deprecated hostname cookbook

--- cookbooks/hostname/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'hostname'
-maintainer       'Rackspace'
-maintainer_email 'steven.gonzales@rackspace.com'
-license          'APLv2'
-description      'Configures /etc/hostname'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
