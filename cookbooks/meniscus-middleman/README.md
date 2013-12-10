4622d773e8e00b362906433e80a5cff7356fa8e2
Chad Lung <chad.lung@gmail.com>
2013-12-10 17:02:04 -0600

    Initial check-in for meniscus-middleman recipe

---
+++ cookbooks/meniscus-middleman/README.md
@@ -0,0 +1,7 @@
+# meniscus-middleman cookbook
+
+A wrapper for the middleman cookbook
+
+# Author
+
+Author:: Chad Lung (<chad.lung@rackspace.com>)
---
+++ cookbooks/meniscus-middleman/metadata.rb
@@ -0,0 +1,8 @@
+name             'meniscus-middleman'
+maintainer       'Chad Lung'
+maintainer_email 'chad.lung@rackspace.com'
+license          'Apache 2.0'
+description      'Installs/Configures meniscus-middleman'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
