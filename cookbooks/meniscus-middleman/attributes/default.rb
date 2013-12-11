078f8bb88fc31a1916fcf47ae2f483752ba3d18c
Chad Lung <chad.lung@gmail.com>
2013-12-10 19:04:48 -0600

    Update default.rb

--- cookbooks/meniscus-middleman/attributes/default.rb
+++ cookbooks/meniscus-middleman/attributes/default.rb
@@ -1,7 +1,2 @@
 normal[:cloudpassage][:server_tag] = "#{node.environment}-middleman"
 normal[:middleman][:uwsgi_socket] = node[:rackspace][:private_ip]
-normal[:middleman][:keystone_endpoint] = 'https://keystone-stg.projectmeniscus.org:35357/v2.0'
-
-middleman_settings = data_bag_item(node.chef_environment, node[:middleman][:databag_item])
-normal[:middleman][:auth_token] = ""
-normal[:middleman][:elasticsearch_endpoint] = ""
