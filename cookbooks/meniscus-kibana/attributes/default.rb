443516f75cd51761cab083cc3b643e3b11ca87bf
Steven Gonzales <stevendgonzales@gmail.com>
2013-12-26 23:54:10 -0600

    update attributes

--- cookbooks/meniscus-kibana/attributes/default.rb
+++ cookbooks/meniscus-kibana/attributes/default.rb
@@ -1,3 +1,6 @@
 default[:meniscus][:personality] = "kibana"
 normal[:cloudpassage][:server_tag] = "#{node.environment}-kibana"
 
+default[:kibana][:webserver_aliases] = [node[:rackspace][:private_ip]]
+default[:kibana][:webserver_listen] = node[:rackspace][:private_ip]
+
